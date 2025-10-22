1d17ca3934b714810f41f6a02d129373c6d6ab04
Vince Michael <vipanton@my.cspc.ed.ph>
2025-10-23 03:03:57 +0800

    inusay ko na

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 // This allows multiple connections to be handled efficiently
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
