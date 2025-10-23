4b0425c275a9b40babd0a55b3cc5987fc6f284fd
Vince Michael <vipanton@my.cspc.ed.ph>
2025-10-23 13:38:34 +0800

    fix cloudinary setup

--- src/config/multer.js
+++ src/config/multer.js
@@ -12,7 +12,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'elaba-images', // folder sa Cloudinary
+    folder: 'elaba-images',
     allowed_formats: ['jpg', 'png'],
   },
 });
--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -4,7 +4,7 @@ const upload = require('../config/multer');
 
 router.post('/upload', upload.single('image'), (req, res) => {
   try {
-    res.json({ url: req.file.path }); // Cloudinary URL ng uploaded image
+    res.json({ url: req.file.path });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Image upload failed' });
